29cadbfeed148913367c443e3a7c1887e6f1c943
suchanun6104101393 <suchanun0209@gmail.com>
2022-11-28 15:31:58 +0700

    devServer

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  transpileDependencies: true,
   devServer: {
-    public: "https://octopus-app-lrdas.ondigitalocean.app:8080",
+    public: "octopus-app-lrdas.ondigitalocean.app",
   },
 });
